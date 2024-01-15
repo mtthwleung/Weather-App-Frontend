f52a4f5a34a187e06ef8097a995c0dab098034d1
mtthwleung@gmail.com <mtthwleung@gmail.com>
2024-01-15 20:45:54 +0800

    added toggle for displaying temperature in fahrenheit

--- src/Components/DisplayArea/DisplayArea.js
+++ src/Components/DisplayArea/DisplayArea.js
@@ -5,15 +5,19 @@ import rainyImage from "../../assets/images/rainy.png";
 import umbrella from "../../assets/images/umbrella.png"
 import droplet from "../../assets/images/droplet.png"
 import Forecast from './Forecast';
+import { useState } from 'react';
 
 export default function DisplayArea({ weatherData }) {
+	
+	const [showFahrenheit, setShowFahrenheit] = useState(false);
 
 	// return if no data loaded yet
 	if (!weatherData) {
 		//loader or spinner here
-		return <p>Loading</p>;
+		return <div className="displayContainer">Loading</div>;
 	}
 
+	//displaying day of the week in the forecast
   const today = new Date();
   const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat",];
 
@@ -22,6 +26,7 @@ export default function DisplayArea({ weatherData }) {
   const day2 = daysOfWeek[(today.getDay() + 2) % 7];
   const day3 = daysOfWeek[(today.getDay() + 3) % 7];
 
+	// assigning the correct image to the corresponding weather
 	const getWeatherImage = weather => {
 		if (weather === 'Sunny') {
 			return sunnyImage;
@@ -37,11 +42,61 @@ export default function DisplayArea({ weatherData }) {
 	const day2Image = getWeatherImage(weatherData.day2.weather);
 	const day3Image = getWeatherImage(weatherData.day3.weather);
 
+	//toggling between celsius and fahrenheit
+	const convertTemperaturesToFahrenheit = weatherData => {
+
+		const convertedWeather = JSON.parse(JSON.stringify(weatherData)); // original data is not mutated
+
+		for (const day in convertedWeather) {
+			const temperatureInCelsius = convertedWeather[day].temperature;
+			const temperatureInFahrenheit = Math.round(temperatureInCelsius * (9 / 5) + 32);
+			convertedWeather[day].temperature = temperatureInFahrenheit;
+
+			const lowTempInCelsius = convertedWeather[day].lowTemp;
+			const lowTempInFahrenheit = Math.round(lowTempInCelsius * (9 / 5) + 32);
+			convertedWeather[day].lowTemp = lowTempInFahrenheit;
+
+			const highTempInCelsius = convertedWeather[day].highTemp;
+			const highTempInFahrenheit = Math.round(highTempInCelsius * (9 / 5) + 32);
+			convertedWeather[day].highTemp = highTempInFahrenheit;
+		}
+		return convertedWeather;
+	};
+
+	const weatherInFahrenheit = convertTemperaturesToFahrenheit(weatherData);
+
+	const setFahrenheit = () => {
+		setShowFahrenheit(true);
+	}
+
+	const setCelsius = () => {
+		setShowFahrenheit(false);
+	}
+
   return (
 			<div className="displayContainer">
 				<div className="todayWeather">
 					<img className="mainImage" src={day0Image} alt="weather icon" />
-					<p className="temperature">{weatherData.day0.temperature}°C</p>
+					<div className="unit">
+						<p className="temperature">
+							{showFahrenheit
+								? weatherInFahrenheit.day0.temperature
+								: weatherData.day0.temperature}
+						</p>
+						<div
+							className={showFahrenheit ? "symbol" : "selected-symbol"}
+							onClick={setCelsius}
+						>
+							°C
+						</div>
+						<div>|</div>
+						<div
+							className={showFahrenheit ? "selected-symbol" : "symbol"}
+							onClick={setFahrenheit}
+						>
+							°F
+						</div>
+					</div>
 					<p className="weather">{weatherData.day0.weather}</p>
 				</div>
 
@@ -61,26 +116,58 @@ export default function DisplayArea({ weatherData }) {
 				<div className="forecastContainer">
 					<Forecast
 						date={day0}
-						highTemp={weatherData.day0.highTemp}
-						lowTemp={weatherData.day0.lowTemp}
+						highTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day0.highTemp
+								: weatherData.day0.highTemp
+						}
+						lowTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day0.lowTemp
+								: weatherData.day0.lowTemp
+						}
 						weatherImage={day0Image}
 					/>
 					<Forecast
 						date={day1}
-						highTemp={weatherData.day1.highTemp}
-						lowTemp={weatherData.day1.lowTemp}
+						highTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day1.highTemp
+								: weatherData.day1.highTemp
+						}
+						lowTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day1.lowTemp
+								: weatherData.day1.lowTemp
+						}
 						weatherImage={day1Image}
 					/>
 					<Forecast
 						date={day2}
-						highTemp={weatherData.day2.highTemp}
-						lowTemp={weatherData.day2.lowTemp}
+						highTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day2.highTemp
+								: weatherData.day2.highTemp
+						}
+						lowTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day2.lowTemp
+								: weatherData.day2.lowTemp
+						}
 						weatherImage={day2Image}
 					/>
 					<Forecast
 						date={day3}
-						highTemp={weatherData.day3.highTemp}
-						lowTemp={weatherData.day3.lowTemp}
+						highTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day3.highTemp
+								: weatherData.day3.highTemp
+						}
+						lowTemp={
+							showFahrenheit
+								? weatherInFahrenheit.day3.lowTemp
+								: weatherData.day3.lowTemp
+						}
 						weatherImage={day3Image}
 					/>
 				</div>
